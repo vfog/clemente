a62fb2aa644084f1c368dfc6413a347cef7ce5d2
Richard C. McGuire <richardcmcguire@gmail.com>
2013-02-13 16:02:24 -0500

    More game parsing.

--- pedro.rb
+++ pedro.rb
@@ -3,6 +3,14 @@ require 'open-uri'
 
 doc = Nokogiri::XML(open('http://gd2.mlb.com/components/game/mlb/year_2012/month_06/day_10/scoreboard.xml'))
 doc.xpath('//go_game').each do |g|
-	game = g.xpath('game')
-	puts game.xpath('@id')
+	puts g.xpath('.//game/@id')
+	g.xpath('.//team').each do |t|
+		team_name = t.xpath('.//@name')
+		runs = t.xpath('.//gameteam/@R')
+		hits = t.xpath('.//gameteam/@H')
+		errors = t.xpath('.//gameteam/@E')
+
+		puts "#{team_name}: #{runs} #{hits} #{errors}"
+	end
+	puts ''
 end
