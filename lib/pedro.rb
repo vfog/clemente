d684f258a111a889a3846ee4114f781924b480f3
Richard C. McGuire <richardcmcguire@gmail.com>
2013-02-13 16:02:24 -0500

    Rearranged directory to satisfy rubygem structure

--- lib/pedro.rb
+++ lib/pedro.rb
@@ -4,11 +4,14 @@ require 'open-uri'
 doc = Nokogiri::XML(open('http://gd2.mlb.com/components/game/mlb/year_2012/month_06/day_10/scoreboard.xml'))
 doc.xpath('//go_game').each do |g|
 	puts g.xpath('.//game/@id')
+	
 	g.xpath('.//team').each do |t|
-		team_name = t.xpath('.//@name')
-		runs = t.xpath('.//gameteam/@R')
-		hits = t.xpath('.//gameteam/@H')
-		errors = t.xpath('.//gameteam/@E')
+		team_name = t.attr('name')
+
+		gt = t.xpath('.//gameteam')
+		runs = gt.attr('R')
+		hits = gt.attr('H')
+		errors = gt.attr('E')
 
 		puts "#{team_name}: #{runs} #{hits} #{errors}"
 	end
