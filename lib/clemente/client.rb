0b842292b3a285c3607bac036eefe5d0fb24e6a6
Richard C. McGuire <richardcmcguire@gmail.com>
2013-02-15 10:25:13 -0500

    refactored client.

--- lib/clemente/client.rb
+++ lib/clemente/client.rb
@@ -8,15 +8,18 @@ class Client
 protected
 
   def fetch(path, date = Date.today)
-    url = get_url(path, date)
-
-    Nokogiri::XML(open(url))
+    doc = get_document_stream(get_url(path, date))    
+    Nokogiri::XML(doc)
   end 
 
 private
 
   def get_url(path, date)
     "http://gd2.mlb.com/components/game/mlb/year_%d/month_%02d/day_%02d/%s" % [date.year, date.month, date.day, path]
+  end
+
+  def get_document_stream(url)
+    open(url)
   end
 
 end
